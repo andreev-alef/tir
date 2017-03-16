942c095c0ab4ffbc023ce1c1b171ea086094a796
andreev_af <andreev_af@10.0.0.10>
2017-03-16 10:06:12 +0700

    1

---
+++ web/lib/fonts/ttf2woff.cmd
@@ -0,0 +1,9 @@
+rem ttf2woff.exe cuprum-regular.ttf cuprum-regular.woff
+rem ttf2woff.exe exo2-light.ttf exo2-light.woff
+rem ttf2woff.exe exo2-regular.ttf exo2-regular.woff
+rem ttf2woff.exe notoserif-regular.ttf notoserif-regular.woff
+rem ttf2woff.exe opensans-light.ttf opensans-light.woff
+rem ttf2woff.exe opensans-regular.ttf opensans-regular.woff
+rem ttf2woff.exe pt_serif-web-regular.ttf pt_serif-web-regular.woff
+
+ttf2woff.exe allegretto_script_two.ttf allegretto_script_two.woff
